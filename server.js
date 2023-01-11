bd4b2895cf61199af5ced974b5568cca567c957f
lbenas <yehuda@benasaraf.com>
2023-01-11 01:07:55 -0500

    command

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const app = express();
+
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = [];
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(flash());
+app.use(session({
+    secret: '4694166',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+
+app.get('/', checkNotAuthenticated, (req, res) => {
+    res.render('load.ejs')
+})
+
+app.get('/home', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
+
+
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true 
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+})
+
+app.get('/water', checkAuthenticated, (req, res) => {
+    res.render('water.ejs');
+})
+
+app.get('/inspire', checkAuthenticated, (req, res) => {
+    res.render('list.ejs');
+})
+app.get('/about', checkAuthenticated, (req,res) => {
+    res.render('about.ejs')
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut((err) => {
+        if (err) { return next(err); };
+        res.redirect('/login');
+    })
+    
+  })
+
+function checkAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return res.redirect('/home')
+    }
+
+    next()
+}
+
+app.listen(3000);
